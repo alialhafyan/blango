c2aa45c6aa33e8aad8d5c7084edb93bf11d74283
Ali Alhafyan <alialhafyan739@gmail.com>
2024-09-19 21:40:50 +0000

    Finish related fields

--- blog/api/serializers.py
+++ blog/api/serializers.py
@@ -1,7 +1,37 @@
 from rest_framework import serializers
-from blog.models import Post
+from blog.models import Post,Tag
+from django.contrib.auth.models import User
+from rest_framework.reverse import reverse
+from .views import UserDetail,PostDetail,PostList
 class PostSerializer(serializers.ModelSerializer):
+    tags = serializers.SlugRelatedField(
+        slug_field="value", many=True,
+        queryset=Tag.objects.all()
+    )
+
+    author_id = serializers.HyperlinkedRelatedField(
+        queryset=User.objects.all(),
+        view_name="api_user_detail")
+        def get_url(self, obj, view_name, request, format):
+            url_kwargs = {
+                'User_email': obj.User.email,
+                'User_pk': obj.pk}
+        
+        return reverse(view_name, kwargs=url_kwargs, request=request, format=format)
+
+        def get_object(self, view_name, view_args, view_kwargs):
+            lookup_kwargs = {
+                'User_email': view_kwargs['User_email'],
+                'User_pk': view_kwargs['User_pk']
+        }
+            return self.get_queryset().get(**lookup_kwargs)
+    
+
     class Meta:
         model = Post
         fields = "__all__"
         readonly = ["modified_at", "created_at"]
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["first_name", "last_name", "email"]
--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,18 @@ from django.db import models
 
 # Create your models here 
 
-
+class Tag(models.Model):
+  value = models.TextField(max_length=100, unique=True)
+  def __str__(self):
+    return self.value
 class Post(models.Model):
     author_id = models.IntegerField() 
     created_at = models.DateTimeField(auto_now_add=True) 
-    modified_at = models.DateTimeField(auto_now=True)   
+    modified_at  = models.DateTimeField(auto_now=True)   
     published_at = models.DateTimeField(auto_now_add=True) 
     title = models.CharField(max_length=255) 
     slug = models.SlugField(unique=True)  
     summary = models.TextField(max_length=1000)  
     content=models.TextField(max_length=3000)
+    tags = models.ManyToManyField(Tag, related_name='posts')
+
